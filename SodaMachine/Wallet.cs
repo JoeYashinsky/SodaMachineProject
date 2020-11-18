49e85ab6ab8ba0e2fa15d86a5e807b64102b5bb5
JoeYashinsky <joey.yashinsky@gmail.com>
2020-11-18 09:41:33 -0500

    Populated the Customer Wallet with coins to start with. 16 quarters ($), 10 dimes (), 20 nickels (), and 25 pennies (/usr/bin/bash.25). Customer was to begin with at least  in mixed change and they now have .25 in their Wallet.

--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -20,6 +20,27 @@ namespace SodaMachine
         //Fills wallet with starting money
         private void FillRegister()
         {
+            for (int i = 0; i < 16; i++)
+            {
+                Quarter quarter = new Quarter();
+                Coins.Add(quarter);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Dime dime = new Dime();
+                Coins.Add(dime);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Nickel nickel = new Nickel();
+                Coins.Add(nickel);
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                Penny penny = new Penny();
+                Coins.Add(penny);
+            }
+
           
         }
     }
